605c1309175a216c01c61786944f4473c4518c28
encyclepdia <79493580+encyclepdia@users.noreply.github.com>
2021-04-12 15:49:41 +0800

    import get from 'lodash/get'

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
+import get from 'lodash/get'
 
 const location = get(this, 'props.location')
 
