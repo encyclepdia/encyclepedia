a4b4de2137ebd7bcb348192965bdf69ef1b50327
encyclepdia <79493580+encyclepdia@users.noreply.github.com>
2021-04-12 15:48:03 +0800

    location={location}

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
+const location = get(this, 'props.location')
+
 const Tags = ({ data }) => {
   return (
-    <Layout>
+    <Layout location={location}>
       <div>Tags</div>
     </Layout>
   );
