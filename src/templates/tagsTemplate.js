650b95f3ae20da94512c0eb3327e4af077142d34
encyclepdia <79493580+encyclepdia@users.noreply.github.com>
2021-04-13 14:31:26 +0800

    Update tagsTemplate.js

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -3,13 +3,72 @@ import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import get from 'lodash/get'
 
-const location = get(this, 'props.location')
 
-const Tags = ({}) => {
-  return (
-      <div>Tags</div>
-  );
-};
+class TagIndex extends React.Component {
+  render() {
+    const siteTitle = get(
+      this,
+      'props.data.cosmicjsSettings.metadata.site_title'
+    )
+    const posts = get(this, 'props.data.allCosmicjsPosts.edges')
+    const author = get(this, 'props.data.cosmicjsSettings.metadata')
+    const location = get(this, 'props.location')
 
-export default Tags;
-
+    return (
+      <Layout location={location}>
+        <Helmet title={siteTitle} />
+        <Bio settings={author} />
+        <h1>test</h1>
+        {posts.map(({ node }) => {
+          const title = get(node, 'title') || node.slug
+          return (
+            <div key={node.slug}>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: 'none' }} to={`posts/${node.slug}`}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.created}</small>
+              <p
+                dangerouslySetInnerHTML={{ __html: node.metadata.description }}
+              />
+            <small>{node.metadata.category}</small>
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+export default TagIndex;
+export const pageQuery = graphql`
+  query IndexQuery {
+    allCosmicjsPosts(sort: { fields: [created], order: DESC }, limit: 1000) {
+      edges {
+        node {
+          metadata {
+            description
+            category
+          }
+          slug
+          title
+          created(formatString: "DD MMMM, YYYY")
+        }
+      }
+    }
+    cosmicjsSettings(slug: { eq: "general" }) {
+      metadata {
+        site_title
+        author_name
+        author_bio
+        author_avatar {
+          imgix_url
+        }
+      }
+    }
+  }
+`
