37263beab0f6985860e2f313e2775468a7071681
encyclepdia <79493580+encyclepdia@users.noreply.github.com>
2021-04-13 13:55:24 +0800

    Update tagsTemplate.js

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -5,10 +5,40 @@ import get from 'lodash/get'
 
 const location = get(this, 'props.location')
 
-const Tags = ({ data }) => {
+const Tags = ({pageContext, data }) => {
   return (
       <div>Tags</div>
+        <h1>{pageContext}</h1>    
   );
 };
 
 export default Tags;
+export const pageQuery = graphql`
+  query IndexQuery {
+    allCosmicjsPosts(sort: { fields: [created], order: DESC }, limit: 1000,
+  filter: { metadata: { category: { in: [$tag] } } }
+) {
+      edges {
+        node {
+          metadata {
+            description
+            category
+          }
+          slug
+          title
+          created(formatString: "DD MMMM, YYYY")
+        }
+      }
+    }
+    cosmicjsSettings(slug: { eq: "general" }) {
+      metadata {
+        site_title
+        author_name
+        author_bio
+        author_avatar {
+          imgix_url
+        }
+      }
+    }
+  }
+`
