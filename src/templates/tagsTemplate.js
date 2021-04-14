6b264d81cac1308a995e9249bca16a8dde8cdb25
encyclepdia <79493580+encyclepdia@users.noreply.github.com>
2021-04-14 15:08:14 +0800

    Update tagsTemplate.js

--- src/templates/tagsTemplate.js
+++ src/templates/tagsTemplate.js
@@ -17,7 +17,7 @@ class TagIndex extends React.Component {
     const posts = get(this, 'props.data.allCosmicjsPosts.edges')
     const author = get(this, 'props.data.cosmicjsSettings.metadata')
     const location = get(this, 'props.location')
-    const tag =  get(this, 'props.data.allCosmicjsPosts.edges.node[0].metadata.category')
+    const tag =  get(this, 'props.data.allCosmicjsPosts.edges[0].node.metadata.category')
     return (
       <Layout location={location}>
         <Helmet title={siteTitle} />
